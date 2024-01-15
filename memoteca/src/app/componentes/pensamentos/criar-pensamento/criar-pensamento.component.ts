6fa747e1fdc4bc66904334026ea7604cb7cb8121
rafael <rafael.rodrigues.bento@outlook.com>
2024-01-15 19:36:10 -0300

    entendendo o fluxo de dados

---
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrl: './criar-pensamento.component.css'
+})
+export class CriarPensamentoComponent {
+
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo3'
+  }
+
+  criarPensamento() {
+    alert('Pensamento criado!')
+  }
+
+  cancelar() {
+    alert('Pensamento cancelado!')
+  }
+
+}
