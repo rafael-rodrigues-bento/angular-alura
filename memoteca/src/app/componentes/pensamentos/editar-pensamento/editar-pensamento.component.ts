00252e134856fe5711f5233a755cd0b050b81cd0
rafael <rafael.rodrigues.bento@outlook.com>
2024-01-27 15:28:46 -0300

    validações em formulários reativos

--- memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -1,5 +1,5 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { Pensamento } from '../pensamento/pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -9,33 +9,50 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrl: './editar-pensamento.component.css'
 })
 export class EditarPensamentoComponent implements OnInit {
-  pensamento: Pensamento = {
-    id: '',
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-  }
 
   constructor (
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) {}
 
+  formulario!: FormGroup;
+
   ngOnInit(): void {
+
+
     const id = this.route.snapshot.paramMap.get('id')
     this.service.buscarPorId(id!).subscribe((pensamento) => {
-      this.pensamento = pensamento
+      this.formulario = this.formBuilder.group({
+        id: [pensamento.id],
+        conteudo: [pensamento.conteudo, Validators.compose([
+          Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)*/)
+        ])],
+        autoria: [pensamento.autoria, Validators.compose([
+          Validators.required,
+          Validators.minLength(3)
+        ])],
+        modelo: [pensamento.modelo]
+      })
     })
   }
 
   editarPensamento() {
-    this.service.editar(this.pensamento).subscribe(() => {
+    this.service.editar(this.formulario.value).subscribe(() => {
       this.router.navigate(['/listarPensamento'])
     })
   }
 
   cancelar() {
     this.router.navigate(['/listarPensamento'])
+  }
+
+  habilitarBotao(): string {
+    if(this.formulario.valid) {
+      return "botao"
+    }
+    else return "botao__desabilitado"
   }
 }
