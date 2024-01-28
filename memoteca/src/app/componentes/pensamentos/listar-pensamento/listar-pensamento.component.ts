2d338c29de5efe08bcb82b8d764d1e571a979c38
rafael <rafael.rodrigues.bento@outlook.com>
2024-01-28 00:01:50 -0300

    implementando paginação

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -5,17 +5,27 @@ import { PensamentoService } from '../pensamento.service';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrl: './listar-pensamento.component.css'
+  styleUrl: './listar-pensamento.component.css',
 })
 export class ListarPensamentoComponent  implements OnInit {
   constructor (private service: PensamentoService) {}
 
   listaPensamentos: Pensamento[] = [];
-
+  paginaAtual: number = 1
+  haMaisPensamentos: boolean = true;
 
   ngOnInit(): void {
-    this.service.listar().subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
+    })
+  }
+
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual).subscribe((listaPensamentos) => {
+      this.listaPensamentos.push(...listaPensamentos);
+      if(!listaPensamentos.length) {
+        this.haMaisPensamentos = false
+      }
     })
   }
 }
