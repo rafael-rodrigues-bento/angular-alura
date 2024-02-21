0c507d405b1727af1a6c5f481e3a9bbd309997f7
rafael <rafael.rodrigues.bento@outlook.com>
2024-02-20 23:30:32 -0300

    consumindo api e validando cep

--- forms-orientados-templates/src/app/cadastro/cadastro.component.ts
+++ forms-orientados-templates/src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ConsultaCepService } from '../service/consulta-cep.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,7 +10,10 @@ import { NgForm } from '@angular/forms';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router, 
+    private consultaCepService: ConsultaCepService
+    ) { }
 
   ngOnInit(): void {
   }
@@ -21,5 +25,24 @@ export class CadastroComponent implements OnInit {
       alert('Formulário inválido!')
     }
       console.log(form.controls);
+  }
+
+  consultaCEP(ev: any, f: NgForm) {
+    const cep = ev.target.value;
+    if(cep !== "") {
+      this.consultaCepService.getConsultaCep(cep).subscribe((resultado) => {
+        this.populandoEndereco(resultado, f)
+      })
+    }
+  }
+
+  populandoEndereco(dados: any, f: NgForm) {
+    f.form.patchValue({
+      endereco: dados.logradouro,
+      complemento: dados.complemento,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      estado: dados.uf,
+    })
   }
 }
